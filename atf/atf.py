9ab8ab6363c75888874128f980e8a711193aa971
shadowyogi <m.iron@icloud.com>
2022-05-11 23:50:39 +0200

    switched to log returns, corr. port. risk

--- atf/atf.py
+++ atf/atf.py
@@ -8,16 +8,18 @@ import numpy as np
 import scipy.optimize as sco
 from binance.spot import Spot as Client
 pd.set_option('display.max_rows', 80)
+pd.options.display.float_format = '{:.4f}'.format
 # pd.reset_option('display.float_format')
 
 # Synchronize with timeserver if time is off, ommit base_url to default to api.binance.com
 client = Client(os.getenv('BINANCE_API_KEY'), os.getenv(
     'BINANCE_API_SECRET'), base_url='https://testnet.binance.vision')
-print(pd.to_datetime(client.time()['serverTime'], unit='ms'))
+print('Server Time')
+print(pd.to_datetime(client.time()['serverTime'], unit='ms'), '\n')
 
 # %%
 balance = pd.json_normalize(client.account()['balances'])
-print(balance)
+print(f'Balance\n{balance}\n')
 
 # %% All kline fields for selected assets
 symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'LTCUSDT', 'TRXUSDT', 'XRPUSDT']
@@ -33,29 +35,15 @@ assets = assets.set_index(pd.to_datetime(assets['Close time', 'BTCUSDT'], unit='
     '%Y-%m-%d'))  # Set close time as index, needs improvement
 assets.index.name = 'Close time'
 assets_close = assets["Close"].copy().astype(float)  # Daily close prices
-print(assets_close)
-
+print(f'Close Price\n{assets_close}\n')
 
 # %% Daily Returns
-returns = assets_close.pct_change().dropna()
-# Maybe better to use logarithimc returns np.log(assets_close / assets_close.shift(1)).dropna().mean()
-print(returns.mean(axis=0))
-
-# %%
-print(returns)
-
-# %% Test
-lreturns = np.log(assets_close / assets_close.shift(1)).dropna()
-lreturns.mean()
-
-# %% Test
-lreturns
+# returns = assets_close.pct_change().dropna()
+returns = np.log(assets_close / assets_close.shift(1)).dropna()
+print(f'Daily Mean Returns\n{returns.mean(axis=0)}\n')
 
 # %% Correlation Coefficient
-print(returns.corr())
-
-# %% Test
-lreturns.corr()
+print(f'Correlation Coefficient\n{returns.corr()}\n')
 
 # %%
 riskfree_return = 0.031  # 5 Year Treasury Rate, but testnet resets every month
@@ -68,7 +56,7 @@ def portfolio_return(weights):
 
 
 def portfolio_risk(weights):
-    return returns.dot(weights.T).std() * 365.25
+    return returns.dot(weights.T).std() * np.sqrt(365.25)
 
 
 def minimized_sharpe(weights):
@@ -83,68 +71,29 @@ bounds = tuple((0, 1) for x in range(asset_qty))
 optimum = sco.minimize(minimized_sharpe, equal_weights,
                        method='SLSQP', bounds=bounds, constraints=constraint)
 np.set_printoptions(suppress=True)
-print(optimum)  # No surprise with that 157.07$ BNB outlier
-
-# %% Test
-riskfree_return = 0.031  # 5 Year Treasury Rate, but testnet resets every month
-# Optimal Sharpe Ratio Portfolio (Tangency Portfolio)
-asset_qty = len(assets_close.columns)
-
-
-def portfolio_return(weights):
-    return lreturns.dot(weights.T).mean() * 365.25
-
-
-def portfolio_risk(weights):
-    return lreturns.dot(weights.T).std() * 365.25
-
-
-def minimized_sharpe(weights):
-    return (riskfree_return - portfolio_return(weights)) / portfolio_risk(weights)
-
-
-equal_weights = np.full(asset_qty, 1 / asset_qty)
-constraint = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
-bounds = tuple((0, 1) for x in range(asset_qty))
-
-# Sequential Least Squares Programming
-optimum = sco.minimize(minimized_sharpe, equal_weights,
-                       method='SLSQP', bounds=bounds, constraints=constraint)
-np.set_printoptions(suppress=True)
-optimum  # No surprise with that 157.07$ BNB outlier
+optimum
 
 # %% Optimal Weights
-pd.options.display.float_format = '{:.4f}'.format
 optimal_weights = optimum['x']
 optimal_weights = pd.Series(index=assets_close.columns, data=optimal_weights)
-print(optimal_weights)
+print(f'Optimal Weights\n{optimal_weights}\n')
 
 # %%
-print(portfolio_return(optimal_weights))
+portfolio_return(optimal_weights)
 
 # %%
-print(portfolio_risk(optimal_weights))
+portfolio_risk(optimal_weights)
 
 # %%
-print(-minimized_sharpe(optimal_weights))
+-minimized_sharpe(optimal_weights)
 
 # %% Tangency Portfolio
 returns['TP'] = returns.dot(optimal_weights)
-print(returns)
-
-# %% Test
-# Tangency Portfolio
-lreturns['TP'] = lreturns.dot(optimal_weights)
-lreturns
+print(f'Daily Returns, including Tangency Portfolio\n{returns}\n')
 
 # %% Covariance
 COV = returns.cov() * 365.25
-print(COV)
-
-# %% Test
-# Covariance
-COV = lreturns.cov() * 365.25
-COV
+print(f'Covariance\n{COV}\n')
 
 # %%
 COV.iloc[:-1, -1].dot(optimal_weights)
@@ -158,10 +107,9 @@ COV.iloc[-1, -1]
 def annualised_risk_return(returns):
     stats = returns.agg(['mean', 'std']).T
     stats.columns = ['Return', 'Risk']
-    # Crypto exchanges trade every day, including leap days.
-    stats.Return = stats.Return * 365.25
-    stats.loc[stats.Return < -1, 'Return'] = -1  # Set losses > 100% to -100%
-    stats.Risk = stats.Risk * np.sqrt(365.25)  # maybe switch to log returns
+    stats.Return = stats.Return * 365.25  # Trading days + 1/4 leap day.
+    # stats.loc[stats.Return < -1, 'Return'] = -1  # Set losses > 100% to -100%
+    stats.Risk = stats.Risk * np.sqrt(365.25)
     return stats
 
 
@@ -179,34 +127,4 @@ stats["CAPM"] = riskfree_return + \
     (stats.loc["TP", "Return"] - riskfree_return) * stats.beta
 # Alpha, asset below or above Security market line
 stats['alpha'] = stats.Return - stats.CAPM
-print(stats)
-
-# %% Test
-# Annualised Risk σ, Return, Sharpe & Variance
-
-
-def annualised_risk_return(lreturns):
-    stats = lreturns.agg(['mean', 'std']).T
-    stats.columns = ['Return', 'Risk']
-    # Crypto exchanges trade every day, including leap days.
-    stats.Return = stats.Return * 365.25
-    # stats.loc[stats.Return < -1, 'Return'] = -1 # Set losses > 100% to -100%
-    stats.Risk = stats.Risk * np.sqrt(365.25)  # maybe switch to log returns
-    return stats
-
-
-# Risk may not be accurate for mean daily losses > 0.274% ()
-stats = annualised_risk_return(lreturns)
-stats['Sharpe'] = stats['Return'].sub(riskfree_return) / stats['Risk']
-stats['Variance'] = np.power(stats.Risk, 2)
-# Systematic & Unsystematic Risk in Variance
-stats['Sys. Var.'] = COV.iloc[:, -1]
-stats['Unsys. Var.'] = stats['Variance'].sub(stats['Sys. Var.'])
-# Normalize == beta
-stats['beta'] = stats['Sys. Var.'] / stats.loc['TP', 'Sys. Var.']
-# Expected Return
-stats["CAPM"] = riskfree_return + \
-    (stats.loc["TP", "Return"] - riskfree_return) * stats.beta
-# Alpha, asset below or above Security market line
-stats['alpha'] = stats.Return - stats.CAPM
-stats
+print(f'Key Measurements\n{stats}')
