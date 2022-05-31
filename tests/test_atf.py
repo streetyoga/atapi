bb9f3afa17e8331aa202c5b120a82ba371e6743c
shadowyogi <m.iron@icloud.com>
2022-05-31 22:54:01 +0200

    added test conditions

--- tests/test_atf.py
+++ tests/test_atf.py
@@ -1,8 +1,19 @@
 """Binance API client"""
 from binance.spot import Spot as Client
+import requests
+MC_BASE = 'https://www.binance.com'
+MC_PATH = '/exchange-api/v2/public/asset-service/product/get-products'
+MC_URL = MC_BASE + MC_PATH  # For Circulating Supply Data
 
 
 def test_connection():
-    """test connection"""
+    """test mainnet API connection"""
     client = Client()
-    client.time()
+    assert 'serverTime' in client.time()
+
+
+def test_supply():
+    """test connection to supply url"""
+    response = requests.get(MC_URL)
+    print(response)
+    assert str(response) == "<Response [200]>"
