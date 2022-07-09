2f6886d4e5219dd66891ec940f2c0bcf6dacc9a4
shadowyogi <m.iron@icloud.com>
2022-07-09 03:41:00 +0200

    parsed circulating supply

---
+++ atapi/app.py
@@ -0,0 +1,20 @@
+from comp.algo import Algo
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def api():
+    return jsonify({'Servertime': Algo.servertime()})
+
+
+@app.route('/api')
+def circulating_supply():
+    parsed = json.loads(Algo.circulating_supply().to_json(orient='index'))
+    return jsonify({"Symbols": parsed})
+
+
+if __name__ == "__main__":
+    app.run(port=47491)
